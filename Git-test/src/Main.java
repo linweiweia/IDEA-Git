2618b2bfdb91bcd963df93b015476f91e21df684
linweiwei <linweiwei@qq.com>
2020-01-19 14:36:57 +0800

    同步开发

--- Git-test/src/Main.java
+++ Git-test/src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        System.out.println("同步开发");
     }
 
     public void weiwei(){
         System.out.println("weiwei");
+        System.out.println("同步开发");
     }
 }
